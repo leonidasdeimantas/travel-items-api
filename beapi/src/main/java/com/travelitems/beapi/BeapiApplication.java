4be941385f8cd92bf4ba5a72debc22cf05ebbc29
Leonidas <45066606+leonidasdeimantas@users.noreply.github.com>
2020-12-19 15:05:33 +0200

    Initialized with Spring Initializr

---
+++ beapi/src/main/java/com/travelitems/beapi/BeapiApplication.java
@@ -0,0 +1,13 @@
+package com.travelitems.beapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeapiApplication.class, args);
+	}
+
+}
---
+++ beapi/src/test/java/com/travelitems/beapi/BeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelitems.beapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
